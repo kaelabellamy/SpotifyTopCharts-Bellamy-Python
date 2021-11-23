2f8684d86a8e9185100e5fbb66f604cd5a02b87b
kbellamy <kaelabellamy@gmail.com>
2021-11-23 16:27:07 -0500

    up to 4 variables; one more then create model

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dataminingproject
+
+You must “engineer” (create) 5 new data columns/fields that are used in your models. Provide rationale as to the reason that you chose the creation of these columns.
+- Column 1: BeginHighWeek
+- Column 2: End High Week
+- Column 3: # of days it took to get to highest charting position aks DaystoTop
+- Column 4: binomial for highest position aka HighestPos
+- Column 5: 
+
+
+# Binomal Regression
+https://timeseriesreasoning.com/contents/binomial-regression-model/
+
+Create multiple models using different variables: DaystoTop, HighestPos, and ... 
+
+Decide best model.
+
+For this project select a dataset that includes a binary, nominal class or ordinal class value that you wish to predict (or a continuous value data element that could be used to create a label to predict) and create a set of “classifiers” (prediction models) that predict the categorical output. 
+
+Determine which methodology/model is the “winner” and provide your reasoning (“I like this method because it’s really cool” is a valid answer, but for full credit I’m looking for something that’s more substantive). 
--- spotify.py
+++ spotify.py
@@ -2,8 +2,8 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+import scipy.stats
 from pandas._libs.missing import NA
-
 from plotnine import *
 
 #%%
@@ -13,34 +13,59 @@ spot = pd.read_csv("C:/Users/kaela/Desktop/datamining/dataminingproject/spotify_
 spot.head()
 
 # %%
+#look at data and do some light cleaning
 spot_c = spot.filter(['Highest Charting Position', 'Week of Highest Charting', 
         'Number of Times Charted', 
-        'Release Date', 'Song Name',  
+        'Release Date', 'Song Name', 'Popularity', 
          'Streams', 'Artist', 'Artist Followers', 'Song ID',
          'Genre', 'Danceability', 'Energy', 'Loudness',
-        'Acousticness', 'Liveness', 'Tempo',
+        'Acousticness', 'Tempo',
          'Duration', 'Valence', 'Chord'])
 spot_c.columns = spot_c.columns.str.replace(' ','_')
 spot_c = spot_c.dropna()
 #%%
 spot_c=spot_c.fillna('')
 spot_c=spot_c.replace(' ', '')
+
+
+### GOAL: TO PREDICT BINARY VALUE OF TOP CHARTS: WILL IT BE IN TOP 20 OR NOT? 
+# 0 = IT IS, 1 = IT IS NOT
+
+
+
 #%%
+###TRYING TO CREATE NUMBER OF DAYS TO TOP AFTER RELEASE DATE - VARIABLE 1,2, and 3
 #check out data set values
 spot_c.Release_Date.value_counts()
 #%%
 #check out data set values
 spot_c.Week_of_Highest_Charting.value_counts()
 #%%
-#split Week of Highest Charting into two new columns 
+#split Week of Highest Charting into two new columns, creates two new variables
 spot_c[['BeginHighWeek', 'EndHighWeek']] = spot_c['Week_of_Highest_Charting'].str.split('--', expand=True)
 #%%
+#convert dates to be recognized as dates
 spot_c['EndHighWeek'] = pd.to_datetime(spot_c['EndHighWeek'], yearfirst=True)
 spot_c['BeginHighWeek'] = pd.to_datetime(spot_c['BeginHighWeek'], yearfirst=True)
 spot_c['Release_Date'] = pd.to_datetime(spot_c['Release_Date'], yearfirst=True)
 # %%
+#find the # of days it took to get to the top for each song aka new column
 spot_c['DaystoTop'] = abs(spot_c['EndHighWeek']-spot_c['Release_Date'])
-#%%
-#spot_c.nsmallest(50,'DaystoTop')
-spot_c['DaystoTop'].astype('timedelta64[s]').plot.bar()
+
+
+
+# %%
+### VARIABLE 4
+#create binomial value for top charts 0 if <= 20 1 if >20 
+spot_c['HighestPos'] = 0 + (spot_c['Highest_Charting_Position'] > 20.0) 
+
+
+
+
+# %%
+### VARIABLE 5
+spot_c['Popularity'] = pd.to_numeric(spot_c['Popularity'],errors = 'coerce')
+
+#scipy.stats.pearsonr(spot_c['Popularity'],spot_c['Artist_Followers'])
+
 # %%
